6bbc691ad8f73e914904a1b835a3b6c9e149676b
Caleb Norton <n0603919@outlook.com>
2024-12-31 16:30:39 -0600

    bring back opts for Comment

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -27,7 +27,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 require('lazy').setup({
 	'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-	'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} }, -- "gc" to comment visual regions/lines
 
 	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		'lewis6991/gitsigns.nvim',
