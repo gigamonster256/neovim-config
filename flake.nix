c750bd267b88001fcc38edc75666a93a2bc3f8c9
Caleb Norton <n0603919@outlook.com>
2025-01-06 15:58:15 -0600

    Flake working

--- flake.nix
+++ flake.nix
@@ -1,31 +1,66 @@
 {
   description = "Neovim config";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nvf.url = "github:NotAShelf/nvf";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs @ {
     systems,
     flake-parts,
+    nvf,
+    git-hooks,
     ...
-  }:
+  }: let
+    buildPkg = pkgs: modules: (nvf.lib.neovimConfiguration {inherit pkgs modules;}).neovim;
+    nvimConfig = import ./config.nix;
+  in
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import systems;
+
       imports = [
-        inputs.git-hooks.flakeModule
+        git-hooks.flakeModule
       ];
+
       perSystem = {
         config,
         pkgs,
+        lib,
         ...
-      }: {
+      }: let
+        neovim = buildPkg pkgs [nvimConfig];
+        neovim-app = lib.meta.getExe neovim;
+      in {
         formatter = pkgs.alejandra;
 
         pre-commit.settings.hooks.alejandra.enable = true;
         devShells.default = config.pre-commit.devShell;
+
+        packages = {
+          inherit neovim;
+          default = neovim;
+        };
+
+        apps = {
+          neovim.program = neovim-app;
+          default.program = neovim-app;
+        };
+      };
+
+      flake.overlays.default = _final: prev: {
+        neovim = buildPkg prev [nvimConfig];
       };
     };
 }
